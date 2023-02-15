0a89fba0686b2e7ed0a8e15739afd31ad419e8d5
kolik <95919407+daskolik@users.noreply.github.com>
2023-02-15 15:24:32 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+local calc = math.random(0 , 284)
+local userid = game.Players.LocalPlayer.UserId
+local stoken = nil --
+local executor = identifyexecutor()
+_G.hwid = nil
+local token = tostring("tokencalc:"..calc.."* id:"..getplayername.."* executor:"..getexecutor.."*hwid ".._G.Hwid)
+print(token)
+
+if calc == nil then
+    error("FATAL ! token calc isnt responded", script, 1)
+end
+
+if stoken = nil then
+   error(" SERVER:FATAL ! script token server isnt responded", script, 3)
+end
